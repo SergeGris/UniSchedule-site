c9a053f778451b15111dfcf0d81d8d66baeca962
LeSaR <ilkov2006@gmail.com>
2023-10-11 14:50:29 +0300

    Added university, faculty, course, group selection, and template /schedule

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory
 from dotenv import load_dotenv
 from os import getenv
 import json
@@ -12,6 +12,30 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/schedule")
+def schedule():
+	university = request.args.get("university")
+	faculty = request.args.get("faculty")
+	course = request.args.get("course")
+	group = request.args.get("group")
+
+	if university is None:
+		return "University not specified", 400
+	if faculty is None:
+		return "Faculty not specified", 400
+	if course is None:
+		return "Course not specified", 400
+	if group is None:
+		return "Group not specified", 400
+
+	return render_template(
+		"schedule.html",
+		university=university,
+		faculty=faculty,
+		course=course,
+		group=group
+	)
 
 
 
--- static/index.js
+++ static/index.js
@@ -1,8 +1,82 @@
 let universities = {};
+let elements = [];
 
-let uni_req = new Request("/static/universities.json")
-fetch(uni_req)
-	.then((response) => response.json())
-	.then((data) => {
-		universities = data;
-	});
+let removeOptions = (index) => {
+
+	let el = elements[index];
+	while (el.children.length > 1)
+		el.removeChild(el.lastChild);
+
+	el.value = "";
+
+}
+
+let modifyOptions = (index, values) => {
+	
+	removeOptions(index);
+	
+	let el = elements[index];
+	for (let value of values) {
+		let option = document.createElement("option");
+		option.innerText = value;
+		option.value = value;
+		el.appendChild(option);
+	}
+
+}
+
+window.onload = () => {
+
+	let uni_req = new Request("/static/universities.json")
+	fetch(uni_req)
+		.then((response) => response.json())
+		.then((data) => {
+
+			universities = data;
+			modifyOptions(0, Object.keys(universities));
+			
+		});
+	
+	elements = [
+		document.getElementById("uni-select"),
+		document.getElementById("faculty-select"),
+		document.getElementById("course-select"),
+		document.getElementById("group-select"),
+		document.getElementById("search-button")
+	];
+
+	for(let i = 0; i < elements.length; i++) {
+		
+		let el = elements[i];
+		el.disabled = true;
+		el.value = "";
+
+		el.addEventListener("change", () => {
+
+			if (el.value == "") {
+				for (let j = i + 1; j < elements.length - 1; j++) removeOptions(j);
+			} else {
+				elements[i + 1].disabled = false;
+
+				if (i + 1 < elements.length - 1) {
+
+					let options = universities;
+					for(let j = 0; j <= i; j++) options = options[elements[j].value];
+					if (!Array.isArray(options)) options = Object.keys(options);
+
+					modifyOptions(i + 1, options);
+
+				}
+
+				for (let j = i + 2; j < elements.length; j++) {
+					elements[j].disabled = true;
+					if (j < elements.length - 1) removeOptions(j);
+				}
+			}
+
+		});
+
+	}
+	elements[0].disabled = false;
+
+}
